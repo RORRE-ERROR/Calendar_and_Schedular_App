45d035b32818548bb76113c08267ada16f2fb2c9
Thet Htun Oakar <24209805@siswa.um.edu.my>
2026-01-19 08:58:31 +0800

    Add files via upload

--- src/app/model/RecurringEvent.java
+++ src/app/model/RecurringEvent.java
@@ -1,7 +1,3 @@
-package app.model;
-
-import java.time.LocalDate;
-
 /*
  * RecurringEvent
  * --------------
@@ -14,75 +10,52 @@ import java.time.LocalDate;
  *
  * This is a model class and contains no logic or file handling.
  */
+
+package app.model;
+
+import java.time.LocalDate; // Date(yyyy-MM-dd)
+
 public class RecurringEvent {
+    /* Variables storing recurrence configuration of an event linked with eventID mirroring the header row of "data/recurrent.csv" file */
 
-    // The ID of the event that this recurrence rule belongs to
-    // This acts as a link (foreign key) to the Event class
-    private int eventId;
+    private int eventId; // Acts as a link to the Event class
 
-    // Defines the recurrence interval
-    // Examples:
-    // "1d" -> repeats every 1 day
-    // "1w" -> repeats every 1 week
-    // "1m" -> repeats every 1 month
-    // "1y" -> repeats every 1 year
-    private String interval;  
-
-    // Number of times the event should repeat
-    // A value of 0 indicates that this option is not used
-    private int recurrentTimes;     // 0 if unused
-
-    // The date on which the recurrence should end
-    // A null value indicates that this option is not used
-    private LocalDate recurrentEndDate; // null if unused
-
-    /*
-     * Constructor for creating a RecurringEvent object.
-     *
-     * Only one of recurrentTimes or recurrentEndDate is typically used
-     * at a time to define the recurrence limit.
+    /* Defines the reccurence interval
+     * Example:
+     * "1d" -> repeats every 1 day
+     * "1w" -> repeats every 1 week
+     * "1m" -> repeats every 1 month
+     * "1y" -> repeats every 1 year
      */
-    public RecurringEvent(int eventId, String interval,
-                          int recurrentTimes, LocalDate recurrentEndDate) {
+    private String recurrentInterval;
 
-        // Assign the associated event ID
+    /* Either recurrentTimes or recurrentEndDate is used at a time*/
+
+    private int recurrentTimes; // Number of times the event should repeat (Inclusive), 0 if unused
+    private LocalDate recurrentEndDate; // The date on which the reccurence should end, null if unused which is written as 0 in the csv file
+
+    /* Constructor is the only way to initialize and set an RecurringEvent object */
+    public RecurringEvent(int eventId, String recurrentInterval, int recurrentTimes, LocalDate recurrentEndDate){
         this.eventId = eventId;
-
-        // Assign the recurrence interval
-        this.interval = interval;
-
-        // Assign the number of recurrence times
+        this.recurrentInterval = recurrentInterval;
         this.recurrentTimes = recurrentTimes;
-
-        // Assign the recurrence end date
         this.recurrentEndDate = recurrentEndDate;
     }
 
-    /*
-     * Returns the ID of the event associated with this recurrence rule.
-     */
-    public int getEventId() {
+    /* Getter Methods for retrieving the value of each instance variable as they are private following the rules of encapsulation */
+    public int getEventId(){
         return eventId;
     }
 
-    /*
-     * Returns the recurrence interval string.
-     */
-    public String getInterval() {
-        return interval;
+    public String getRecurrentInterval(){
+        return recurrentInterval;
     }
 
-    /**
-     * Returns the number of times the event should recur.
-     */
-    public int getRecurrentTimes() {
+    public int getRecurrentTimes(){
         return recurrentTimes;
     }
 
-    /**
-     * Returns the end date of the recurrence.
-     */
-    public LocalDate getRecurrentEndDate() {
+    public LocalDate getRecurrentEndDate(){
         return recurrentEndDate;
     }
 }
